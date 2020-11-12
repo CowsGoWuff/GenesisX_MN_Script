72d7a5f61217a216f2ac4647776c1e9667663a81
CowsGoWuff <20851921+CowsGoWuff@users.noreply.github.com>
2020-11-12 18:17:03 -0500

    Update rocket.sh

--- rocket.sh
+++ rocket.sh
@@ -30,78 +30,78 @@ echo
 echo
 echo
 echo
-sleep 3
+sleep 2
 echo -e "         ! "
-sleep 3
+sleep 2
 echo -e "         !"
-sleep 3
+sleep 2
 echo -e "         ^"
-sleep 3
+sleep 2
 echo -e "        / \\ "
-sleep 3
+sleep 2
 echo -e "       /___\\ "
-sleep 3
+sleep 2
 echo -e "      |=   =|"
-sleep 3
+sleep 2
 echo -e "      |     |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  G ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  E ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  N ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  E ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  S ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  I ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  S ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}    ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}  X ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}    ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |${BLUE}    ${CLEAR} |"
-sleep 3
+sleep 2
 echo -e "      |     |"
-sleep 3
+sleep 2
 echo -e "      |     |"
-sleep 3
+sleep 2
 echo -e "      |     |"
-sleep 3
+sleep 2
 echo -e "      |     |"
-sleep 3
+sleep 2
 echo -e "     /|##!##|\\"
-sleep 3
+sleep 2
 echo -e "    / |##!##| \\"
-sleep 3
+sleep 2
 echo -e "   /  |##!##|  \\"
-sleep 3
+sleep 2
 echo -e "  | /  ( | )  \\ |"
-sleep 3
+sleep 2
 echo -e "  |  / ^ | ^ \\  |"
-sleep 3
+sleep 2
 echo -e "  |/   ( | )   \\|"
-sleep 3
+sleep 2
 echo -e "      ((   ))     ${GREEN} |It was either a rocket, or watch an error  |" ${CLEAR}
-sleep 3
+sleep 2
 echo -e "     ((  :  ))    ${GREEN} | repeat itself while the wallet loads      |" ${CLEAR}
-sleep 3
+sleep 2
 echo -e "     ((   ))      ${GREEN} |  Seeing Error 28 shortly is expected.     |" ${CLEAR}
-sleep 3
+sleep 2
 echo -e "       (( ))      ${GREEN} |               Relax                       |" ${CLEAR}
-sleep 3
+sleep 2
 echo -e "        ( )"
-sleep 3
+sleep 2
 echo -e "         .X^"
-sleep 3
+sleep 2
 echo -e "         .G^"
-sleep 3
+sleep 2
 echo -e "         .S^"
-sleep 3
+sleep 2
 echo -e "         .^"
 echo -e "         ."
